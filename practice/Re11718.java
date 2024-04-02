5eb2e21e3329af2748d9a13012fbba3bc6d00c69
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-02 23:41:23 +0900

    백준 11718_그대로 출력하기
    100글자를 넘지 않는 줄을 입력받기 때문에 scanner.nextLine( )으로 입력을 받아야 하며, 입력받는만큼 그대로 출력하기 때문에 얼마나 입력받는지 모르기에 while 반복문 사용하기. 입력받았는지 확인하여 있으면 true, 없으면 false로 인식해야하기에 while(scanner.hasnext){ } 로 써야한다.

---
+++ practice/Re11718.java
@@ -0,0 +1,26 @@
+package RePractice;
+
+import java.util.Scanner;
+
+/**
+ * packageName : RePractice
+ * fileName : Re11718
+ * author : hayj6
+ * date : 2024-04-02(002)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-02(002)         hayj6          최초 생성
+ */
+public class Re11718 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            String a = scanner.nextLine();
+            System.out.println(a);
+        }
+    }
+}
