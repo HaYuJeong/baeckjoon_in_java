e944b9420a26cea4b40a45390b516c7785293fa1
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-09 00:50:26 +0900

    백준 27323_직사각형
    정수 A, B의 입력을 받고, 두 수의 곱을 구한다.

---
+++ practice/Re27323.java
@@ -0,0 +1,25 @@
+package RePractice;
+
+import java.util.Scanner;
+
+/**
+ * packageName : RePractice
+ * fileName : Re27323
+ * author : hayj6
+ * date : 2024-04-09(009)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-09(009)         hayj6          최초 생성
+ */
+public class Re27323 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();  // A
+        int b = scanner.nextInt();  // B
+        System.out.println(a*b);
+    }
+}
