bc386820fd7f503c2b38c1abfcff4330be6e7884
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-17 01:12:40 +0900

    백준 10718_We love kriii
    입력없는 출력을 할 수 있다.

---
+++ practice/Re10718.java
@@ -0,0 +1,21 @@
+package RePractice;
+
+/**
+ * packageName : RePractice
+ * fileName : Re10718
+ * author : hayj6
+ * date : 2024-04-17(017)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-17(017)         hayj6          최초 생성
+ */
+public class Re10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
