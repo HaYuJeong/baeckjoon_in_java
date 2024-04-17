43a9baae7046b7ac5c95c01a6ffcd31aa7db3bd1
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-17 23:26:44 +0900

    백준 1000_a+b
    입력받는 두 정수의 합을 출력한다.

---
+++ practice/Re1000.java
@@ -0,0 +1,25 @@
+package RePractice;
+
+import java.util.Scanner;
+
+/**
+ * packageName : RePractice
+ * fileName : Re1000
+ * author : hayj6
+ * date : 2024-04-17(017)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-17(017)         hayj6          최초 생성
+ */
+public class Re1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();  // 1
+        int b = scanner.nextInt();  // 2
+        System.out.println(a + b);
+    }
+}
