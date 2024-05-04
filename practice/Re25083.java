db006805cd18f572c57aae9e4336b6b52ccfdd90
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-05-04 10:01:06 +0900

    25083_새싹
    입력없이 바로 출력하기

---
+++ practice/Re25083.java
@@ -0,0 +1,27 @@
+package RePractice;
+
+import java.util.Scanner;
+
+/**
+ * packageName : RePractice
+ * fileName : Re27323
+ * author : hayj6
+ * date : 2024-04-09(009)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-09(009)         hayj6          최초 생성
+ */
+public class Re25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
