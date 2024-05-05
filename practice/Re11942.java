4bd7e814e0147de4fc110d744740912a7e3de2ee
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-05-05 21:40:47 +0900

    고려대는 사랑입니다_11942
    입력없이 출력

---
+++ practice/Re11942.java
@@ -0,0 +1,23 @@
+package RePractice;
+
+import java.util.Scanner;
+
+/**
+ * packageName : RePractice
+ * fileName : Re27323
+ * author : hayj6
+ * date : 2024-04-09(009)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-09(009)         hayj6          최초 생성
+ */
+public class Re11942 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("고려대학교");
+    }
+}
